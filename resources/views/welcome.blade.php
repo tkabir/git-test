3dc1d8c72dbe2b17477d1751cbcd3b73f5067702
tkabir <mtkabir285@gmail.com>
2016-12-28 00:07:42 +0600

    added index, changed welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome To Laravel
                 </div>
 
                 <div class="links">
